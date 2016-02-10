2954c551aea21986963640ebda1efcfb88505859
Joell Shepperson <joellshepperson@Joells-MacBook-Pro.local>
2016-02-09 19:21:44 -0500

    updated work to include proper code!
    It works!

--- citypix/js/citypix.js
+++ citypix/js/citypix.js
@@ -2,46 +2,34 @@ $(document).ready(function() {
 
 // set the dropdown values and create variables for the new dropdown choices and values of those choices
 	var dropdown = ["NYC", "SF", "LAX", "ATX", "SYD"];
-	var newOption = '';
 
 // fill dropdown list with choices from the array by appending a new option row
 	for (var i=0;i<dropdown.length;i++) {
-   		newOption = ('<option value="'+ dropdown[i] + '">' + dropdown[i] + '</option>');
-   		$('#cities').append(newOption);
+   		$('#cities').append('<option value="'+ dropdown[i] + '">' + dropdown[i] + '</option>');
    	}
 
- });
+// change background to match users selection
+   	$('#cities').change(changeStyle);
 
-// Note: this section seems to break the other good work of populating the dropdown so I wrote it in the comments
+   	function changeStyle () {
+   		var choice = $('select').val();
+   		console.log(choice);
 
-/* 
-create event to notify when it's time to change the background picture
-
-	$('cityChoice').submit(changeCity);
-
-change the background based on which option the user selects
-
-	function changeCity () {
-		var chosenCity = $('#cities').val();
-
-		if (chosenCity == "nyc") {
+   		if (choice == dropdown[0]) {
 			$("body").css('background-image', 'url(images/nyc.jpg)');
 		
-		} else if (chosenCity == 'sf') {
+		} else if (choice == dropdown[1]) {
 			$("body").css('background-image', 'url(images/sf.jpg)');
 		
-		} else if (chosenCity == 'lax') {
+		} else if (choice == dropdown[2]) {
 			$("body").css('background-image', 'url(images/la.jpg)');
-
-		} else if (chosenCity == 'atx') {
+		
+		} else if (choice == dropdown[3]) {
 			$("body").css('background-image', 'url(images/austin.jpg)');
 		
-		} else if (chosenCity == 'syd') {
+		} else if (choice == dropdown[4]) {
 			$("body").css('background-image', 'url(images/sydney.jpg)');
-		
-		} else {
-			console.log("help");
-		}
+		} 
+   	}
 
 });
-*/
